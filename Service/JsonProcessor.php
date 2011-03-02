f22f082c7d6da2e439cf581f8d3c3ce1d696bdad
trapp <rapp@equinoxe.info>
2011-03-02 13:57:08 +0100

    Added method comment.

--- Service/JsonProcessor.php
+++ Service/JsonProcessor.php
@@ -4,6 +4,12 @@ namespace Equinoxe\SimpleOutputBundle\Service;
 
 class JsonProcessor implements ProcessorInterface
 {
+    /**
+     * Converts an array to a json string.
+     *
+     * @param array $array The array to encode.
+     * @return string The encoded array.
+     */
     function convert($array) {
         return json_encode($array);
     }
